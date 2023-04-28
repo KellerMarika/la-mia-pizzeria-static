fed521d4becb68ae29baada6ec1f3842dbe630d7
KellerMarika <marikakeller92@gmail.com>
2023-04-28 15:55:51 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()=> View();
+        
+    }
+}
